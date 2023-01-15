2a48548bb7583004effd4f04954caa4062ccc2dc
dexter <49933542+Aman-Khan@users.noreply.github.com>
2023-01-16 00:00:56 +0530

    Update 01 Matrix.cpp

--- 01 Matrix.cpp
+++ 01 Matrix.cpp
@@ -1,3 +1,9 @@
+//Approach 
+//create visited matrix and fill with maximum value(INT_MAX)
+//traverse the given matrix and store all 0 position with a step 0 in queue. (because for 0 nearest 0 aviable is him self) and also edit the value 0 in visited matrix;
+//Now, till queue is not empty and for evey value in queue check all for direction if its not 0 then check for value in visited matrix if the step value in less than
+//already entered value then update a new value in visited matrix.
+
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
